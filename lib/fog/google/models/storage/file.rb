003eebbab90299fc18dd5afd141c0ab9d13e9571
Дмитро Будник <dmitriy.budnik@gmail.com>
2015-09-06 22:49:00 +0300

    Do not check ACL as it seems broken

--- lib/fog/google/models/storage/file.rb
+++ lib/fog/google/models/storage/file.rb
@@ -90,20 +90,10 @@ module Fog
 
         def public_url
           requires :directory, :key
-
-          acl = service.get_object_acl(directory.key, key).body['AccessControlList']
-          access_granted = acl.find do |entry|
-            entry['Scope']['type'] == 'AllUsers' && entry['Permission'] == 'READ'
-          end
-
-          if access_granted
-            if directory.key.to_s =~ /^(?:[a-z]|\d(?!\d{0,2}(?:\.\d{1,3}){3}$))(?:[a-z0-9]|\.(?![\.\-])|\-(?![\.])){1,61}[a-z0-9]$/
-              "https://#{directory.key}.storage.googleapis.com/#{key}"
-            else
-              "https://storage.googleapis.com/#{directory.key}/#{key}"
-            end
+          if directory.key.to_s =~ /^(?:[a-z]|\d(?!\d{0,2}(?:\.\d{1,3}){3}$))(?:[a-z0-9]|\.(?![\.\-])|\-(?![\.])){1,61}[a-z0-9]$/
+            "https://#{directory.key}.storage.googleapis.com/#{key}"
           else
-            nil
+            "https://storage.googleapis.com/#{directory.key}/#{key}"
           end
         end
 
